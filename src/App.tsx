b41d8bfe4ef1292a743096a0445d190af190ad2e
Piotrek <Swistekxd@gmail.com>
2024-03-13 17:59:19 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {
   RouterProvider,
   createRoutesFromElements,
   Route,
-  Routes,
   createBrowserRouter,
 } from "react-router-dom";
 import RootLayout from "./Pages/RootLayout";
@@ -10,15 +9,16 @@ import Error from "./Pages/Error";
 import HomePage from "./Pages/HomePage";
 import DetailsPage from "./Pages/DetailsPage";
 import { loader as dropdownLoader } from "./Components/Dropdown";
+
 const routers = (
   <Route
     path='/'
     element={<RootLayout />}
     loader={dropdownLoader}
-    errorElement={<HomePage />}
+    errorElement={<Error />}
   >
-    <Route index element={<HomePage />} />
-    <Route path='/details/:id' index element={<DetailsPage />} />
+    <Route index element={<HomePage />} /> /
+    <Route path='details/:id' element={<DetailsPage />} />
   </Route>
 );
 
